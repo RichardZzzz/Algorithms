92146eda3c6f3f73700b19fd4b91864ff7b8bc1b
Richarder <zhangruiqian5@gmail.com>
2018-08-25 13:06:48 -0400

    Updated question number and level

--- Copy_List_with_Random_Pointer.java
+++ Copy_List_with_Random_Pointer.java
@@ -1,5 +1,6 @@
 import java.util.HashMap;
 
+// 136 Easy
 // A linked list is given such that each node contains an additional random pointer which could point to any node in the list or null.
 // Return a deep copy of the list.
 /**
