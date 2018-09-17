4abd761dd73d6c6044a04e650ba14d97e2babc6f
Richarder <zhangruiqian5@gmail.com>
2018-09-16 20:05:30 -0400

    add second solution with no back tracking

--- Subsets.java
+++ Subsets.java
@@ -1,23 +1,39 @@
 import java.util.ArrayList;
+import java.util.List;
 
 // 78 Medium
 // Given a set of distinct integers, nums, return all possible subsets (the power set).
 class Solution {
-    public List<List<Integer>> subsets(int[] nums) {
+    public List<List<Integer>> subsets_Solution1(int[] nums) {
         List<List<Integer>> list = new ArrayList();
-        backTrack(list, new ArrayList(), nums, 0);
+        backTrack_Solution1(list, new ArrayList(), nums, 0);
         return list;
     }
 
-    private void backTrack(List<List<Integer>> list, ArrayList<Integer> temp, int[] nums, int start){
+    private void backTrack_Solution1(List<List<Integer>> list, ArrayList<Integer> temp, int[] nums, int start){
         // add current temp into list
         list.add(new ArrayList(temp));
         for(int i=start; i<nums.length; i++){
             // add next element to temp
             temp.add(nums[i]);
-            backTrack(list, temp, nums, i+1);
+            backTrack_Solution1(list, temp, nums, i+1);
             // remove last element from temp
             temp.remove(temp.size()-1);
         }
     }
+
+    public List<List<Integer>> subsets_Solution2 (int[] nums){
+        List<List<Integer>> list = new ArrayList();
+        list.add(new ArrayList());
+        for(int n:nums){
+            int size = list.size();
+            for(int i=0;i<size;i++){
+                List<Integer> subset = new ArrayList(list.get(i));
+                subset.add(n);
+                list.add(subset);
+            }
+        }
+        return list;
+    }
 }
+
